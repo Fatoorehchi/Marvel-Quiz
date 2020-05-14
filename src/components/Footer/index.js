8951d23a16d3aab581c303c1a5d4ebc108bcceda
Fatoorehchi <tfatoo@gmail.com>
2020-05-14 08:07:19 +0200

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>Projet réalisé par Tom Fatoorehchi - mai 2020</p>
+                <p>Les icones Wolverine, iron Man, Spider Man, Batman sont prises sur iconFinder.com</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
